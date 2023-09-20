d674fa82de24994686a3544426d1201d27109466
Julio <julio.mech@hotmail.com>
2023-09-20 11:40:57 -0300

    create UI test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
+
 describe('My First Test', () => {
   it('Does not do much!', () => {
-    expect(true).to.equal(true)
+    expect(true).to.equal(false)
+  })
+})
+
+describe('template spec', () => {
+  it('passes', function () {
+    cy.visit('https://www.amazon.com.br/')
   })
 })
---
+++ cypress/e2e/spec2.cy.js
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('passes', function () {
+    cy.visit('https://www.google.com.br/')
+  })
+})
